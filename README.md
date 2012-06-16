c73691c7e204183e85194d46f5626c32dafe0c14
jzbl <jzbl@server.fake>
2012-06-16 15:46:10 +0200

    Minimal working interpreter. - README.md changes. - Minor CMake script version changes. - License setup.

--- README.md
+++ README.md
@@ -5,10 +5,24 @@ Description
 --------------------
 
 Simple brainfuck interpreter that reads sourcecode from file and executes it.
-It has a small memory limitation of 4kb.
+It has a spec memory limitation of 3k cells, and original undefined behaviour when you do stupid stuff.
 
 Requirements
 --------------------
 
-- Cmake (to build)
+- *Cmake* (to build)
+
+or
+
+`# gcc sbf.c -o sbf`
+
+Usage
+--------------------
+
+`./sbf source.bf`
+
+License
+--------------------
+
+[WTFPL](http://sam.zoy.org/wtfpl/COPYING)
 
--- sbf.c
+++ sbf.c
@@ -1,14 +1,51 @@
 #include <stdio.h>
 
-#define MEMSIZE 1024*4
+#define MEMSIZE 30000
+#define BF_RIGHT '>'
+#define BF_LEFT '<'
+#define BF_INC '+'
+#define BF_DEC '-'
+#define BF_OUT '.'
+#define BF_IN ','
+#define BF_LOOPS '[' 
+#define BF_LOOPE ']'
 
 FILE *srcfile;
+int srclen = 0;
 char src[MEMSIZE];
 char mem[MEMSIZE];
-char *ip = mem;
+char *ip = src;
+char *dp = mem;
 
-void interpret() {
-  
+int interpret() {
+  for (;;) { 
+  switch (*ip) {
+    case BF_RIGHT:
+      ++dp;
+      break;
+    case BF_LEFT:
+      --dp;
+      break;
+    case BF_INC:
+      ++(*dp);
+      break;
+    case BF_DEC:
+      --(*dp);
+      break;
+    case BF_OUT:
+      putchar(*dp);
+      break;
+    case BF_IN:
+      *dp=(int)getchar();
+      break;
+  }
+  if (srclen == 0)
+    break;
+  else
+    ip++;
+    srclen--;
+  }
+  return 0;
 }
 
 int main(int argc, char *argv[] ) {
@@ -23,13 +60,16 @@ int main(int argc, char *argv[] ) {
     else {
       int c = 0;
       int buf;
-      int i = 0;
+      /* int i = 0; */
       while ( ( buf = fgetc(srcfile) ) != EOF ) {
         src[c] = buf;
         c++;
       }
-      for (i = 0; i < c; i++)
-        printf("%c", src[i]);
+      srclen = c;
+      /* for (i = 0; i < c; i++)
+        printf("%c", src[i]); */
+      interpret();
+      printf("\n");
     }
   }
 	return 0;
